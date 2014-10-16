d893bbf976f68d91747829d4c3d04533531518dc
David DeTomaso <davedeto@gmail.com>
2014-10-15 19:11:00 -0700

    Initial Changes to Compile in Fortran

--- LJFluid.f
+++ LJFluid.f
@@ -5,29 +5,25 @@ c
 c
 
       program LJFluid
-      integer a, b, c
-      real*8 blahblah
-      
-      real*8, allocatable :: derivs(:,:)
-
-      integer N_PARTICLES = 216
-      real*8 mp = 18/1000
-      real*8 dt = 1e-6
-      integer nstep = 10000
+      implicit none
+      integer :: N_PARTICLES = 216
+      real*8 :: mp = 18.0/1000
+      real*8 :: dt = 1e-6
+      integer :: nstep = 10000
 
 c     constants
-      real*8 kb = 1.3806488e-23
-      real*8 av = 6.0221413e23
+      real*8 :: kb = 1.3806488e-23
+      real*8 :: av = 6.0221413e23
 
 c     For lennard-jones potential
 
-      real*8 e_lj = .16275*4184
-      real*8 sigma_lj = 3.16435/10;
+      real*8 :: e_lj = .16275*4184
+      real*8 :: sigma_lj = 3.16435/10;
 
 c     Box dimensions
-      real*8 xbox = 2.2
-      real*8 ybox = 2.2
-      real*8 zbox = 2.2
+      real*8 :: xbox = 2.2
+      real*8 :: ybox = 2.2
+      real*8 :: zbox = 2.2
 
 c     arrays to hold position, velocity, and energy derivatives
 
@@ -42,10 +38,34 @@ c     arrays to hold position, velocity, and energy derivatives
       real*8, allocatable :: derivs(:,:)
       real*8, allocatable :: derivs_old(:,:)
 
-      integer T
+      integer T, i, j, k
       T = nint(N_PARTICLES**(1.0/3.0))
 
+      allocate(x(N_PARTICLES))
+      allocate(y(N_PARTICLES))
+      allocate(z(N_PARTICLES))
+
+      allocate(vx(N_PARTICLES))
+      allocate(vy(N_PARTICLES))
+      allocate(vz(N_PARTICLES))
+
+      allocate(derivs(N_PARTICLES,3))
+      allocate(derivs_old(N_PARTICLES,3))
+
+      
       do i = 1, T
-         print*, i
+         do j = 1,T
+            do k = 1,T
+               ii = (i-1)*T*T + (j-1)*T + k
+               x(ii) = xbox/T*i
+               y(ii) = ybox/T*j
+               z(ii) = zbox/T*k
       end do
+
+      do i = 1, N_PARTICLES
+         vx(i) = 0
+         vy(i) = 0
+         vz(i) = 0
+      end do
+
       end
